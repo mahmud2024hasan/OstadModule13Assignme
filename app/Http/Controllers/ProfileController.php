4fe29a31f379993cfddedd0c0f8ae4e55e48e114
Mahmud Hasan <mahmud23hasan@gmail.com>
2024-12-13 21:58:32 +0600

    Add new repo

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+        // User Details
+        $id = $request->id;
+        $name = "Donald Trump";
+        $age = "78";
+        $data = ["id" => $id, "name" => $name, "age" => $age];
+        
+        // Set Cookie
+        $cookie_name = "access_token";
+        $value = "123-XYZ";
+        $minutes = 1;
+        $path = "/";
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response($data, 200)->cookie($cookie_name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+        
+    }
+}
